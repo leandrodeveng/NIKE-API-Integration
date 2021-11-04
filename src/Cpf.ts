2d869018aa41d37fdfd63c9b2a21fd0c69fa931c
leandro <leandrotrindade.lemobs@gmail.com>
2021-11-03 22:47:03 -0300

    Testing non-digit characters

--- src/Cpf.ts
+++ src/Cpf.ts
@@ -2,10 +2,15 @@ export class Cpf {
     private value: string;
 
     constructor(cpf: string) {
+        const cpfDigits = this.extractDigits(cpf);
         this.value = cpf
     }
 
     getCpf() {
         return this.value;
+    }
+
+    private extractDigits(cpf: string) {
+        return cpf.replace(/\D/g, "");
     }
 }
