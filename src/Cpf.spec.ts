f2fb603a03f02859ba6623496e55b1237fa42e7f
leandrodeveng <leandro.lt187@gmail.com>
2021-11-04 00:18:43 -0300

    Formating project

--- src/Cpf.spec.ts
+++ src/Cpf.spec.ts
@@ -1,26 +1,24 @@
-import { Cpf } from "./Cpf";
+import { Cpf } from './Cpf';
 
-beforeEach(() => {
-
-});
+beforeEach(() => {});
 
 test('Should return the CPF value', () => {
-    const validCpf = '464.972.498-85'
-    const cpf = new Cpf(validCpf);
-    expect(cpf.getCpf()).toBe("464.972.498-85");
+  const validCpf = '464.972.498-85';
+  const cpf = new Cpf(validCpf);
+  expect(cpf.getCpf()).toBe('464.972.498-85');
 });
 
 test('Should return wrong cpf length exception', () => {
-    const invalidCpf = '4644.972.498-85'
-    expect(() => new Cpf(invalidCpf)).toThrow(new Error("Invalid CPF length"));
+  const invalidCpf = '4644.972.498-85';
+  expect(() => new Cpf(invalidCpf)).toThrow(new Error('Invalid CPF length'));
 });
 
 test('Should return wrong digits exception', () => {
-    const invalidCpf = '123.972.498-85'
-    expect(() => new Cpf(invalidCpf)).toThrow(new Error("Invalid check digits"));
+  const invalidCpf = '123.972.498-85';
+  expect(() => new Cpf(invalidCpf)).toThrow(new Error('Invalid check digits'));
 });
 
 test('Should return invalid digits exception', () => {
-    const invalidCpf = '111.111.111-11'
-    expect(() => new Cpf(invalidCpf)).toThrow(new Error("Invalid CPF digits"));
+  const invalidCpf = '111.111.111-11';
+  expect(() => new Cpf(invalidCpf)).toThrow(new Error('Invalid CPF digits'));
 });
