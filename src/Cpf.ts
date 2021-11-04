4e5070ff5a09da70cc834fc082fe7acf1babae10
leandro <leandrotrindade.lemobs@gmail.com>
2021-11-03 23:30:58 -0300

    Calculating CPF digits

--- src/Cpf.ts
+++ src/Cpf.ts
@@ -1,4 +1,8 @@
-const CPF_WITHOUT_DIGIT_LENGTH = 11;
+const CPF_VALID_LENGTH = 11;
+const FACTOR_FIRST_DIGIT = 10;
+const FACTOR_SECOND_DIGIT = 11;
+const MAX_DIGITS_FIRST = 9;
+const MAX_DIGITS_SECOND = 10;
 
 export class Cpf {
     private cpfValue: string;
@@ -6,6 +10,8 @@ export class Cpf {
     constructor(cpf: string) {
         const cpfDigits = this.extractDigits(cpf);
         if(this.isInvalidLength(cpfDigits)) throw new Error('Invalid CPF length');
+        const firstDigit = this.calculateDigit(cpf, FACTOR_FIRST_DIGIT, MAX_DIGITS_FIRST);
+        const secondDigit = this.calculateDigit(cpf, FACTOR_SECOND_DIGIT, MAX_DIGITS_SECOND);
         this.cpfValue = cpf
     }
 
@@ -18,6 +24,15 @@ export class Cpf {
     }
 
     private isInvalidLength(cpf: string) {
-        return cpf.length !== CPF_WITHOUT_DIGIT_LENGTH;
+        return cpf.length !== CPF_VALID_LENGTH;
+    }
+
+    private calculateDigit(cpf: string, factor: number, max: number) {
+        let total = 0;
+        const digitArray = [...cpf].map(digit => parseInt(digit))
+        for(const digit of digitArray.slice(0, max)) {
+            total += digit * factor--;
+        }
+        return (total % 11 < 2) ? 0 : (11 - total % 11);
     }
 }
