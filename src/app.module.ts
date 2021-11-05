0faa3655e502cbf2607d8ce397cb8fb004d9a48a
leandrodeveng <leandro.lt187@gmail.com>
2021-11-05 20:05:14 -0300

    Adding swagger to document API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,4 @@
-import { ClientOrdersHttpService } from './clientOrdersHttp.service';
-import { HttpModule, HttpService } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { ClientOrderController } from './clientOrders.controller';
-import { ClientOrdersService } from './clientOrders.service';
 import { ClientOrdersModule } from './clientOrders.module';
 
 @Module({
