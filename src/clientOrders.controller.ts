329147e905d7657d7eaf1656da77791692559b4c
leandrodeveng <leandro.lt187@gmail.com>
2021-11-06 16:52:06 -0300

    Improving swagger doc

--- src/clientOrders.controller.ts
+++ src/clientOrders.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import { ApiHeader, ApiOperation, ApiParam, ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ClientOrdersService } from './clientOrders.service';
 import { Cpf } from './Cpf';
 import { GetProducts } from './GetProducts';
@@ -10,7 +10,12 @@ import { ListProductsOutputData } from './ListProductOutputData';
 export class ClientOrderController {
 	constructor(private readonly clientOrdersService: ClientOrdersService) {}
 
-	@Get(':cpf')
+	@Get(':cpf/last-products')
+	@ApiParam({ name: 'cpf', required: true, description: 'Client CPF (format: xxx.xxx.xxx-xx)' })
+	@ApiResponse({ status: 200, description: 'The products has successfully returned'})
+	@ApiResponse({ status: 400, description: 'Something went wrong with the CPF sended'})
+	@ApiResponse({ status: 500, description: 'Internal server error. Please contact the support'})
+	@ApiOperation({ description: 'Should return the last 5 products of client orders and the count' })
 	async findProducts(
 		@Param('cpf') cpfIntupData: string,
 	): Promise<ListProductsOutputData> {
